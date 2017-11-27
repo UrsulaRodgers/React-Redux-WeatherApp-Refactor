3fd4d252b9f4133f410e41d2cfdf357692a242f5
ursula_rodgers <ursula.r.rodgers@gmail.com>
2017-11-27 14:59:07 +0000

    updated Default and index.css. Clean-up

--- src/components/Default.js
+++ src/components/Default.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+
 import CurrentDate from './CurrentDate';
 import icon from '../icon.json';
 import wind from '../images/wind_24.png';
 import humidity from '../images/humidity.png';
+
+const API_KEY = '8d1dab70d6486ad4b46fe911084f46af';
 
 class Default extends Component {
   constructor (props) {
@@ -13,7 +16,8 @@ class Default extends Component {
   		currentLocation: '',
   		country: '',
   		currentTemp:'',
-  		icon: icon
+  		icon: icon,
+      forecast: []
   	};
 
   }
@@ -23,24 +27,44 @@ class Default extends Component {
   	const location = await axios.get('http://ip-api.com/json');
   	const lat = location.data.lat;
   	const lon = location.data.lon;
-  	const API_KEY = '8d1dab70d6486ad4b46fe911084f46af';
   	const currentWeather = await axios.get(`http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}`);
+    const forecast = await axios.get(`http://api.openweathermap.org/data/2.5/forecast/daily?lat=${lat}&lon=${lon}&cnt=7&appid=${API_KEY}`);
+
   	
   	this.setState ({
   		currentLocation: location.data.city,
   		country: location.data.countryCode,
-  		currentTemp: Math.round(currentWeather.data.main.temp- 273),
+  		currentTemp: Math.round(currentWeather.data.main.temp - 273),
   		minTemp: Math.round(currentWeather.data.main.temp_min - 273),
   		maxTemp: Math.round(currentWeather.data.main.temp_max - 273),
   		wind: currentWeather.data.wind.speed,
   		humidity: currentWeather.data.main.humidity,
   		weatherIcon: currentWeather.data.weather[0].icon,
-  		description: currentWeather.data.weather[0].main
+  		description: currentWeather.data.weather[0].main,
+      forecast: forecast.data.list
   	});
-  		
   }
-  
+
   render() {
+   console.log(this.state.forecast)
+
+   const forecastBlock = this.state.forecast.slice(1).map((value,index) => {
+        return (
+          <div className="col-md-2 text-center" key={index}>
+            <div>
+              <strong>{(new Date(value.dt*1000)).toDateString().substring(0,3)}</strong>
+            </div>
+            <div>
+              <img className="forecastIcon" alt='' src={this.state.icon[value.weather[0].icon]} />
+            </div>
+              <div className="temps">
+                <span className="highTemp">{Math.round(value.temp.max - 273)}&#8451;</span>&nbsp;&nbsp;&nbsp;
+                &nbsp;&nbsp;&nbsp;<span className="lowTemp">{Math.round(value.temp.min - 273)}&#8451;</span>
+              </div>
+            </div>
+          )
+    })
+
     return (
       <div className="container">
         <h2 className="text-center">{this.state.currentLocation}, {this.state.country}</h2>
@@ -56,10 +80,13 @@ class Default extends Component {
         			&nbsp;<span><strong>LOW</strong> {this.state.minTemp}</span></p>
         			<p className="description">{this.state.description}</p>
         			<p className="otherIcons"><img src={wind}/>
-					&nbsp;<span>{this.state.wind} mph</span> | <img src={humidity}/>
-					<span>{this.state.humidity}</span></p>
-				</div>
+					     &nbsp;<span>{this.state.wind} mph</span> | <img src={humidity}/>
+					     <span>{this.state.humidity}</span></p>
+				    </div>
         	</div>
+        </div>
+        <div className="row forecast">
+          {forecastBlock}
         </div>
       </div>
     );
--- src/reducers/index.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from 'redux';
-import DefaultLocation from './reducer_default_location';
-
-const rootReducer = combineReducers({
-  myLocation: DefaultLocation
-/*key:value pair where key is the piee of state, and value is the value of that state - the reducer*/
-});
-
-export default rootReducer;
